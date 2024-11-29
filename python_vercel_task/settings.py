a6b5924dfab01a316a3eddc215ed84e1d3705bb6
Joaquín Sosa <joaquinsosab@gmail.com>
2024-11-29 12:21:23 -0300

    v10

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
